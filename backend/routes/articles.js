f65bb449e56704bd107e17a793f3788e3f93ac60
TonyMckes <tonymckes@gmail.com>
2022-01-31 17:35:53 -0400

    Fixed article queries by tags

--- backend/routes/articles.js
+++ backend/routes/articles.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const verifyToken = require("../middleware/authentication");
 const { slugify } = require("../helper/slugify");
 const { Article } = require("../models");
 const { User } = require("../models");
@@ -9,7 +10,7 @@ router.get("/", async (req, res) => {
   try {
     const { author, tag /* TODO: favorited  */ } = req.query;
 
-    const articles = await Article.findAll({
+    const articleList = await Article.findAll({
       include: [
         {
           model: Tag,
@@ -26,12 +27,41 @@ router.get("/", async (req, res) => {
       ],
     });
 
-    for (const article of articles) {
-      const tagList = [];
-      for (const tag of article.dataValues.tagList) {
-        tagList.push(tag.name);
+    //* Tests failing because: https://github.com/gothinkster/realworld/issues/839
+    const articles = [];
+    for (let article of articleList) {
+      // TODO: I need to refactor this;
+      if (tag) {
+        article = await Article.findOne({
+          where: { id: article.id },
+          include: [
+            {
+              model: Tag,
+              as: "tagList",
+              attributes: ["name"],
+            },
+            {
+              model: User,
+              as: "author",
+              attributes: ["username", "bio", "image" /* "following" */],
+            },
+          ],
+        });
       }
-      article.dataValues.tagList = tagList;
+
+      const tagList = [];
+      if (tag) {
+        for (const tag of article.dataValues.tagList) {
+          tagList.push(tag.name);
+          article.dataValues.tagList = tagList;
+        }
+      } else {
+        for (const tag of article.dataValues.tagList) {
+          tagList.push(tag.name);
+          article.dataValues.tagList = tagList;
+        }
+      }
+      articles.push(article);
     }
 
     res.json({ articles, articlesCount: articles.length });
@@ -40,8 +70,15 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.post("/", async (req, res) => {
+router.post("/", verifyToken, async (req, res) => {
   try {
+    const userData = req.user;
+    const user = await User.findOne({
+      attributes: ["id", "username", "bio", "image" /* "following" */],
+      where: { email: userData.email },
+    });
+    if (!user) throw new Error("You need to login first!");
+
     const data = req.body.article;
 
     const slug = slugify(data.title);
@@ -51,9 +88,26 @@ router.post("/", async (req, res) => {
       title: data.title,
       description: data.description,
       body: data.body,
+      userId: user.id,
     });
 
-    res.json(article);
+    for (const tag of data.tagList) {
+      const checkTag = await Tag.findByPk(tag);
+
+      if (!checkTag) {
+        const tagList = await Tag.create({ name: tag });
+
+        await article.addTagList(tagList);
+      } else {
+        await article.addTagList(checkTag);
+      }
+    }
+
+    article.dataValues.tagList = data.tagList;
+
+    article.dataValues.author = user;
+
+    res.json({ article });
   } catch (error) {
     res.json({ errors: error.message });
   }
