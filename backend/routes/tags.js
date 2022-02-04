a133b09c269c9f2c58f66236da4cc27e498b272a
TonyMckes <tonymckes@gmail.com>
2022-02-04 09:31:36 -0400

    Fixed function name

--- backend/routes/tags.js
+++ backend/routes/tags.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const { Tag } = require("../models");
-const { printTagList } = require("../helper/helpers");
+const { appendTagList } = require("../helper/helpers");
 
 // All Tags
 router.get("/", async (req, res) => {
   try {
     const tagList = await Tag.findAll();
 
-    const tags = printTagList(tagList);
+    const tags = appendTagList(tagList);
 
     res.json({ tags });
   } catch (error) {
