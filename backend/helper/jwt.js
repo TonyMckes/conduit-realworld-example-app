4bab97ea64ce259acaab3fd0771d9b0b082d5e92
TonyMckes <tonymckes@gmail.com>
2022-02-21 12:08:28 -0400

    Comments cleanup

--- backend/helper/jwt.js
+++ backend/helper/jwt.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const privateKey = "superSecret"; //! Temporary key
+const privateKey = process.env.JWT_KEY;
 
 module.exports.jwtSign = async (payload) => {
   return jwt.sign(
@@ -7,25 +7,6 @@ module.exports.jwtSign = async (payload) => {
     privateKey,
   );
 };
-
-// module.exports.jwtSign = (payload) => {
-//   try {
-//     return jwt.sign(
-//       { username: payload.username, email: payload.email },
-//       privateKey,
-//     );
-//   } catch (error) {
-//     console.log(`Error signing JWT: ${error}`);
-//   }
-// };
-
-// module.exports.jwtVerify = async (token) => {
-//   try {
-//     return jwt.verify(token, privateKey);
-//   } catch (error) {
-//     console.log(`Error decoding JWT: ${error}`);
-//   }
-// };
 
 module.exports.jwtVerify = async (token) => {
   return jwt.verify(token, privateKey);
