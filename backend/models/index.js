c0ee9bfa5fdf9befaca4c01c4f338678581fb72d
Lucas Vieira <vieiralucas4@gmail.com>
2023-01-26 10:40:41 -0300

    setup ergomake

--- backend/models/index.js
+++ backend/models/index.js
@@ -8,6 +8,7 @@ const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.js")[env];
 const db = {};
 
+console.log(JSON.stringify(config,null,2))
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
