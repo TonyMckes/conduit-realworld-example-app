7d6a1d55715518f15289b2219ed7bebcea57538d
Anthony Mackensen <tonymckes@gmail.com>
2023-03-22 11:50:43 -0400

    refactor: change vite port to 3000

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 3000,
     proxy: {
       '/api': {
         target: 'http://localhost:3001'
