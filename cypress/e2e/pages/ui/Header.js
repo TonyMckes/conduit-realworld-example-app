8d82283c5e3653fa1963e152525c33e758842161
andreinalivaika <nav.testsw@gmail.com>
2023-07-04 12:45:14 +0300

    06 add delete article test

--- cypress/e2e/pages/ui/Header.js
+++ cypress/e2e/pages/ui/Header.js
@@ -1,9 +1,19 @@
+import { profile } from "../index"
+
 class Header {
     signUpLocator = '.navbar a[href$="register"]'
     loginLocator = '.navbar a[href$="login"]'
     userDropdownLocator = '.navbar .dropdown'
     logoutLocator = '.dropdown-menu a[href$="#/"]'
     newArticleLocator = '.navbar a[href$="editor"]'
+    profileLocator = '.dropdown-menu a[href*="#/profile"]'
+
+    openProfilePage(name) {
+        cy.get(this.userDropdownLocator).click()
+        cy.get(this.profileLocator).click()
+        cy.url().should('include', profile.profileUrl)
+        profile.checkProfileTitle(name)
+    }
 }
 
 export const header = new Header()
