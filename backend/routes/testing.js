63b10aa1a35fec43d6c04a29ef6c428c5ef28d68
RoopeK <35376295+Toopemus@users.noreply.github.com>
2024-02-24 20:30:01 +0200

    fixed await on reset route, added template test

--- backend/routes/testing.js
+++ backend/routes/testing.js
@@ -5,7 +5,7 @@ const { sequelize } = require("../models");
 // Reset database
 router.get("/", async (req, res, next) => {
   console.log("Clearing database");
-  sequelize.sync({ force: true });
+  await sequelize.sync({ force: true });
   res.json({ message: "Database has been cleared for testing" });
 });
 
