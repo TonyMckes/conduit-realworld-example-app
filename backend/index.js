809879df1c4bf863349a40e8357bb4de1d009a6e
RoopeK <35376295+Toopemus@users.noreply.github.com>
2024-02-24 19:39:54 +0200

    add route for initializing database for testing

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,7 @@ const userRoutes = require("./routes/user");
 const articlesRoutes = require("./routes/articles");
 const profilesRoutes = require("./routes/profiles");
 const tagsRoutes = require("./routes/tags");
+const testingRoutes = require("./routes/testing");
 
 const app = express();
 app.use(cors());
@@ -30,16 +31,20 @@ if (process.env.NODE_ENV === "production") {
 } else {
   app.get("/", (req, res) => res.json({ status: "API is running on /api" }));
 }
+if (env === "development") {
+  console.log("Toimii");
+  app.use("/api/testing/reset", testingRoutes);
+}
 app.use("/api/users", usersRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/articles", articlesRoutes);
 app.use("/api/profiles", profilesRoutes);
 app.use("/api/tags", tagsRoutes);
 app.get("*", (req, res) =>
-  res.status(404).json({ errors: { body: ["Not found"] } }),
+  res.status(404).json({ errors: { body: ["Not found"] } })
 );
 app.use(errorHandler);
 
 app.listen(PORT, () =>
-  console.log(`Server running on http://localhost:${PORT}`),
+  console.log(`Server running on http://localhost:${PORT}`)
 );
